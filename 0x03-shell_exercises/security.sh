99c57efe1f3ce59610c1345cacba2f04326e5a85
Agancode254 <jezreelsimbarare@gmail.com>
2024-03-14 16:15:21 +0300

    Changes

--- 0x03-shell_exercises/security.sh
+++ 0x03-shell_exercises/security.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-# Count the total number of commands in the .bash_history file
+
+#Count the total number of commands in the .bash_history file
 total_commands=$(wc -l < ~/.bash_history)
 
-#  Count the number of 'echo' commands in the .bash_history file
+#Count the number of 'echo' commands in the .bash_history file
 echo_commands=$(grep -c '^echo' ~/.bash_history)
 
-# Calculate the percentage rate
+#Calculate the percentage rate
 if [ "$total_commands" -eq 0 ]; then
     echo "No commands found in .bash_history"
 else
-    percentage_rate=$(echo "scale=2; ($echo_commands / $total_commands) * 100" |b)
-    # Print the percentage rate with '%' character at the end
+    percentage_rate=$(echo "scale=2; ($echo_commands / $total_commands) * 100" | bc)
+    #Print the percentage rate with '%' character at the end
     echo "Percentage rate of 'echo' commands: $percentage_rate%"
 fi
+
 
 
