1fa7734f48a4c1929432c7b695f0fe9a5a6ad386
Agancode254 <jezreelsimbarare@gmail.com>
2024-03-14 15:16:10 +0300

    changes

--- 0x03-shell_exercises/conditionals.sh
+++ 0x03-shell_exercises/conditionals.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-echo "Enter a number" && read input && (( input % 2 == 0 )) && echo "$input is a multiple of 2" || echo "$input is not a multiple of 2"
+
+# Prompt the user to enter a number
+echo "Enter a number:"
+read input
+
+# Check if the input number is a multiple of 2
+if (( input % 2 == 0 )); then
+    echo "$input is a multiple of 2"
+else
+    echo "$input is not a multiple of 2"
+fi
+
+
 
