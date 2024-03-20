20ac1944dfe9e60799f07a7e2fa2c53929b76522
Agancode254 <jezreelsimbarare@gmail.com>
2024-03-20 16:31:43 +0300

    changes

---
+++ 0x00.C-Hello,/6-size.c.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+int main() { printf("Size of char: %zu byte(s)\nSize of int: %zu byte(s)\nSize of short: %zu byte(s)\nSize of long: %zu byte(s)\nSize of long long: %zu byte(s)\nSize of float: %zu byte(s)\nSize of double: %zu byte(s)\nSize of long double: %zu byte(s)\n", sizeof(char), sizeof(int), sizeof(short), sizeof(long), sizeof(long long), sizeof(float), sizeof(double), sizeof(long double)); return 0; }
+
