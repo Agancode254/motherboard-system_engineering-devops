dc5e92a929c2e7eb21175c680ebaba9bfbef8b6a
Agancode254 <jezreelsimbarare@gmail.com>
2024-03-20 15:04:21 +0300

    Changes

--- C/README.md.sh
+++ C/README.md.sh
@@ -5,7 +5,7 @@ This repository is designed to provide learners with a comprehensive introductio
 
 ## Contents
 
-- **Class Presentation**: Step-by-step illustrations covering basic C concepts such as variables, data types, control flow, functions, arrays, pointers, and more.
+- **Presentation**: Step-by-step illustrations covering basic C concepts such as variables, data types, control flow, functions, arrays, pointers, and more.
 - **Examples**: Code examples demonstrating how to implement various C programming concepts.
 - **Exercises**: Programming exercises to practice and reinforce your understanding of C concepts.
 - **Resources**: Additional resources, including recommended books, online courses, and websites for further learning shared by the mentor.
@@ -13,6 +13,14 @@ This repository is designed to provide learners with a comprehensive introductio
 ## Getting Started
 
 To get started with learning C, simply clone this repository to your local machine and explore the contents.
+Why C programming is awesome
+Who invented C
+Who are Dennis Ritchie, Brian Kernighan and Linus Torvalds
+What happens when you type gcc main.c
+What is an entry point
+What is main
+How to print text using printf, puts and putchar
+
 ## Contributions
 
 Contributions to this repository are welcome! If you have suggestions for improving existing tutorials, adding new examples or exercises, or additional resources to share, feel free to open a pull request or submit an issue.
